00df5947591f6482390ddccfb59f403981bc5261
Dannie Vinther <davi@kea.dk>
2021-10-03 19:34:08 +0200

    remove h1 splitting

--- assets/scroll.js
+++ assets/scroll.js
@@ -33,23 +33,3 @@ articles.forEach((article) => {
     },
   });
 });
-
-const span = (text, index) => {
-  const node = document.createElement("span");
-
-  node.innerText = text;
-  node.style.setProperty("--index", index);
-
-  return node;
-};
-
-const byLetter = (text) => [...text].map(span);
-
-const splitTargets = document.querySelectorAll("[data-split-letters]");
-
-splitTargets.forEach((node) => {
-  let nodes = null;
-  nodes = byLetter(node.innerText);
-
-  if (nodes) node.firstChild.replaceWith(...nodes);
-});
