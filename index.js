29ac2382c5232b00da6ab04db6a7a875fc7df4e1
daviatkea <54393843+daviatkea@users.noreply.github.com>
2022-01-30 16:29:26 +0100

    cs

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-// ======================================= //
-// FIRST
-// ======================================= //
-const btn1 = document.querySelector("#btn-1");
-// din kode her
+console.clear()
+
+// First
+const btn1 = document.querySelector('#btn-1'),
+      box1 = document.querySelector('.box-1')
 
 function clickHandler1() {
-  // din kode her
+  box1.classList.add('js-1')
 }
 
-btn1.addEventListener("click", clickHandler1);
+btn1.addEventListener('click', clickHandler1)
 
-// ======================================= //
-// SECOND
-// ======================================= //
-const btn2 = document.querySelector("#btn-2");
-// din kode her
+
+// Second
+const btn2 = document.querySelector('#btn-2'),
+      boxes = document.querySelectorAll('.box')
 
 function clickHandler2() {
-  // din kode her
+  boxes.forEach( box => {
+    box.classList.add('js-2')
+  })
 }
 
-btn2.addEventListener("click", clickHandler2);
+btn2.addEventListener('click', clickHandler2)
 
-// ======================================= //
-// THIRD
-// ======================================= //
-const btn3 = document.querySelector("#btn-3");
-// din kode her
+
+// Third
+const btn3 = document.querySelector('#btn-3'),
+      headings = document.querySelectorAll('.heading-box')
 
 function clickHandler3() {
-  // din kode her
+  headings.forEach( box => {
+    const heading = box.querySelector('h5')
+    heading.classList.add('js-3')
+  })
 }
 
-btn3.addEventListener("click", clickHandler3);
+btn3.addEventListener('click', clickHandler3)
 
-// ======================================= //
-// FOURTH
-// ======================================= //
-const btn4 = document.querySelector("#btn-4");
-// din kode her
+
+// Fourth
+const btn4 = document.querySelector('#btn-4'),
+      cards = document.querySelectorAll('.card')
 
 function clickHandler4() {
-  // din kode her
+  cards.forEach( card => {
+    const children = card.querySelectorAll(':scope > *')
+    children.forEach(child => {
+      child.classList.add('js-4')
+    })
+  })
 }
 
-btn4.addEventListener("click", clickHandler4);
+btn4.addEventListener('click', clickHandler4)
